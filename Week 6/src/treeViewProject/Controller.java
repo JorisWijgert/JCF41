638d0ff5bb9bd85e9d6d082c9704f56a9d420dce
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-04-01 21:42:11 +0200

    github

--- Week 6/src/treeViewProject/Controller.java
+++ Week 6/src/treeViewProject/Controller.java
@@ -1,12 +1,16 @@
 package treeViewProject;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.TextField;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,22 +26,44 @@ public class Controller implements Initializable {
     @FXML
     TextField tfPlayerAdd;
     @FXML
+    TextField tfPlayerAge;
+    @FXML
+    TextField tfPlayerPosition;
+    @FXML
+    TextField tfPlayerNation;
+    @FXML
     ComboBox cbDivisions;
     @FXML
     ComboBox cbTeams;
+    @FXML
+    TextField tfNaamEdit;
+    @FXML
+    TextField tfAgeEdit;
+    @FXML
+    TextField tfPositionEdit;
+    @FXML
+    TextField tfNationEdit;
+    @FXML
+    TextField tfTeamEdit;
+
+    @FXML
+    TableView<Player> tvPlayers;
 
     public ObservableList<Team> teamObservableList;
     public ObservableList<Division> divisionObservableList;
     public ObservableList<Player> playerObservableList;
+    public Player oldPlayer;
+    public Team oldTeam;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         List<Player> players = new ArrayList();
-        players.add(new Player("Qunfong Wang"));
-        players.add(new Player("Joris van de Wijgert"));
-        players.add(new Player("Henk"));
-        players.add(new Player("Kees"));
+        players.add(new Player("Qunfong Wang", 20, "Spits", "Nederland"));
+        players.add(new Player("Joris van de Wijgert", 19, "Keeper", "Nederland"));
+        players.add(new Player("Henk Janssen", 28, "Verdediging", "België"));
+        players.add(new Player("Kees Hendriks", 14, "Mid", "Frankrijk"));
         playerObservableList = FXCollections.observableList(players);
 
         List<Team> teams = new ArrayList();
@@ -49,34 +75,72 @@ public class Controller implements Initializable {
         divisions.add(new Division("Eredivisie", teamObservableList));
         divisions.add(new Division("Eerste divisie", FXCollections.observableArrayList(new ArrayList<Team>())));
         divisionObservableList = FXCollections.observableList(divisions);
-
-        fillComboBoxes();
-        /*
-        teamObservableList.addListener(new ListChangeListener<Team>() {
+        trvDivisions.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<Object>>() {
             @Override
-            public void onChanged(Change<? extends Team> c) {
-                renewTreeView(divisionObservableList);
+            public void changed(ObservableValue<? extends TreeItem<Object>> observable, TreeItem<Object> oldValue, TreeItem<Object> newValue) {
+                Platform.runLater(() -> {
+                    if (newValue != null && newValue.getValue() instanceof Team) {
+                        oldTeam = (Team) newValue.getValue();
+                        tfTeamEdit.setText(oldTeam.getName());
+                        tvPlayers.setItems((ObservableList<Player>) oldTeam.getChildren());
+                    } else {
+                        tfTeamEdit.clear();
+                    }
+                });
             }
         });
 
-        playerObservableList.addListener(new ListChangeListener<Player>(){
-           @Override
-            public void onChanged(Change<? extends Player>c){
-               renewTreeView(divisionObservableList);
-           }
-        });*/
+        tvPlayers.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Player>() {
+            @Override
+            public void changed(ObservableValue<? extends Player> observable, Player oldValue, Player newValue) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(tvPlayers.getSelectionModel().getSelectedItem() !=null) {
+                            oldPlayer = tvPlayers.getSelectionModel().getSelectedItem();
+                            tfNaamEdit.setText(oldPlayer.getName());
+                            tfAgeEdit.setText(String.valueOf(oldPlayer.getAge()));
+                            tfPositionEdit.setText(oldPlayer.getPosition());
+                            tfNationEdit.setText(oldPlayer.getNation());
+                        }
+                    }
+                });
+            }
+        });
+        fillComboBoxes();
         renewTreeView(divisionObservableList);
     }
 
+    @FXML
+    private void editTableView(){
+        for(Player player : playerObservableList){
+            if(player.equals(oldPlayer)){
+                player.setName(tfNaamEdit.getText());
+                player.setAge(Integer.parseInt(tfAgeEdit.getText()));
+                player.setPosition(tfPositionEdit.getText());
+                player.setNation(tfNationEdit.getText());
+            }
+        }
+        Platform.runLater(() -> renewTreeView(divisionObservableList));
+    }
+
+    @FXML
+    private void editTeam(){
+        for (Team team : teamObservableList){
+            if(team.equals(oldTeam)){
+                team.setName(tfTeamEdit.getText());
+            }
+        }
+        Platform.runLater(() -> renewTreeView(divisionObservableList));
+    }
+
     private void renewTreeView(List<Division> divisions) {
+        tvPlayers.setItems(null);
         TreeItem<Object> divisionTree = new TreeItem("Divisies");
         for (Division division : divisions) {
             TreeItem<Object> divisionTreeItem = new TreeItem(division);
-            for (Team team : division.getChildren()){
+            for (Team team : division.getChildren()) {
                 TreeItem<Object> teamTreeItem = new TreeItem(team);
-                for (Player player : team.getChildren()){
-                    teamTreeItem.getChildren().add(new TreeItem(player));
-                }
                 divisionTreeItem.getChildren().add(teamTreeItem);
             }
             divisionTree.getChildren().add(divisionTreeItem);
@@ -84,29 +148,35 @@ public class Controller implements Initializable {
         trvDivisions.setRoot(divisionTree);
     }
 
+
     private void fillComboBoxes() {
         cbDivisions.setItems(divisionObservableList);
         cbTeams.setItems(teamObservableList);
 
     }
 
-    public void addTeam(){
+    public void addTeam() {
         String teamName = tfTeamAdd.getText();
-        Division toaddDivision = (Division)cbDivisions.getSelectionModel().getSelectedItem();
-        toaddDivision.addObject(new Team(teamName,FXCollections.observableArrayList(new ArrayList<Player>())));
+        Division toaddDivision = (Division) cbDivisions.getSelectionModel().getSelectedItem();
+        toaddDivision.addObject(new Team(teamName, FXCollections.observableArrayList(new ArrayList<Player>())));
         renewTreeView(divisionObservableList);
-        /*
-        for(Division division : divisionObservableList){
-            if(division.getName().equals(toaddDivision.getName())){
 
-            }
-        }*/
     }
 
-    public void addPlayer(){
+    public void addPlayer() {
         String playerName = tfPlayerAdd.getText();
+        int playerAge = Integer.parseInt(tfPlayerAge.getText());
+        String playerPostion = tfPlayerPosition.getText();
+        String playerNation = tfPlayerNation.getText();
         Team toAddTeam = (Team) cbTeams.getSelectionModel().getSelectedItem();
-        toAddTeam.addObject(new Player(playerName));
-        renewTreeView(divisionObservableList);
+        toAddTeam.addObject(new Player(playerName, playerAge, playerPostion, playerNation));
+        clearPlayerFields();
+    }
+
+    private void clearPlayerFields() {
+        tfPlayerAdd.clear();
+        tfPlayerAge.clear();
+        tfPlayerNation.clear();
+        tfPlayerPosition.clear();
     }
 }
--- Week 6/src/treeViewProject/Player.java
+++ Week 6/src/treeViewProject/Player.java
@@ -5,9 +5,15 @@ package treeViewProject;
  */
 public class Player {
     private String name;
+    private int age;
+    private String position;
+    private String nation;
 
-    public Player(String name) {
+    public Player(String name, int age, String position, String nation) {
         this.name = name;
+        this.age = age;
+        this.position = position;
+        this.nation = nation;
     }
 
     public String getName() {
@@ -16,6 +22,30 @@ public class Player {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
     }
 
     @Override
